028f2d771bbed728d18513a10243196a89f2261b
Yers <eabuov4@gmail.com>
2024-04-19 23:53:35 +0500

    some css + input field

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
     <h1>CRYPTO</h1>
+    <Input />
 </template>
 
 <script>
+import Input from './components/Input.vue'
 
+export default {
+    components: { Input }
+}
 </script>
 
 <style scoped>
---
+++ src/components/Input.vue
@@ -0,0 +1,24 @@
+<template>
+    <input type="number">
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+input {
+    outline: none;
+    width: 500px;
+    position: relative;
+    top: -50px;
+    font-size: 1.2em;
+    border-radius: 3px;
+    border: 0px;
+    padding: 10px 15px;
+    background: #fafafa;
+    color: #333;
+}
+</style>
